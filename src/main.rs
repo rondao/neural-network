b11a50b26b54c41c5019443d2b9e6423435bda50
Rafael Rondao <rafael.rondao@gmail.com>
2021-03-29 22:25:44 -0300

    Read images and labels into structured vector
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,72 @@
 use plotters::prelude::*;
-use std::convert::TryInto;
+use std::{convert::TryInto, u32, usize};
 use std::fs;
 
+struct TrainingImage {
+    pixels: Vec<u8>,
+    label: u8,
+}
+
 fn main() {
-    let training_images_file =
-        fs::read("/home/rondao/train-images-idx3-ubyte").expect("Could not read images file.");
+    let (num_rows, num_columns, mut images_pixels) = get_training_images();
+    let mut images_labels = get_training_labels();
 
-    let magic_number = u32::from_be_bytes(training_images_file[..4].try_into().unwrap());
-    let number_of_images = u32::from_be_bytes(training_images_file[4..8].try_into().unwrap());
-    let number_of_rows =
-        u32::from_be_bytes(training_images_file[8..12].try_into().unwrap()) as usize;
-    let number_of_columns =
-        u32::from_be_bytes(training_images_file[12..16].try_into().unwrap()) as usize;
+    let mut training_images:Vec<TrainingImage> = Vec::new();
+    while let (Some(pixels),Some(label)) = (images_pixels.pop(), images_labels.pop()) {
+        training_images.push(TrainingImage{pixels, label});
+    }
 
-    println!(
-        "[{}] {} ({},{})",
-        magic_number, number_of_images, number_of_rows, number_of_columns
-    );
 
-    let image_size = number_of_rows * number_of_columns;
-    let training_images = &training_images_file[16..];
-
-    for (idx, image_bytes) in training_images.chunks(image_size).enumerate() {
+    for (idx, training_image) in training_images.iter().enumerate() {
         let file_name = &format!("plotted-{}.png", idx);
+        println!("{} - {}", file_name, training_image.label);
         let root_drawing_area =
-            BitMapBackend::new(file_name, (number_of_rows as u32, number_of_columns as u32))
+            BitMapBackend::new(file_name, (num_rows as u32, num_columns as u32))
                 .into_drawing_area();
-        let sub_areas = root_drawing_area.split_evenly((number_of_rows, number_of_columns));
+        let sub_areas = root_drawing_area.split_evenly((num_rows, num_columns));
 
-        for row in 0..number_of_rows {
-            for column in 0..number_of_columns {
-                let color = image_bytes[row * number_of_rows + column];
-                sub_areas[row * number_of_rows + column]
+        for row in 0..num_rows {
+            for column in 0..num_columns {
+                let color = training_image.pixels[row * num_rows + column];
+                sub_areas[row * num_rows + column]
                     .fill(&RGBColor(color, color, color))
                     .unwrap();
             }
         }
     }
+}
+
+fn get_training_images() -> (usize, usize, Vec<Vec<u8>>) {
+    let training_images_file =
+        fs::read("/home/rondao/train-images-idx3-ubyte").expect("Could not read images file.");
+
+    assert_eq!(
+        u32::from_be_bytes(training_images_file[..4].try_into().unwrap()),
+        2051
+    );
+    let _num_images = u32::from_be_bytes(training_images_file[4..8].try_into().unwrap());
+
+    let num_rows = u32::from_be_bytes(training_images_file[8..12].try_into().unwrap()) as usize;
+    let num_columns = u32::from_be_bytes(training_images_file[12..16].try_into().unwrap()) as usize;
+
+    let image_size = (num_rows * num_columns) as usize;
+    let images_pixels: Vec<Vec<u8>> = training_images_file[16..]
+        .chunks(image_size)
+        .map(|x| x.to_vec())
+        .collect();
+
+    (num_rows, num_columns, images_pixels)
+}
+
+fn get_training_labels() -> Vec<u8> {
+    let training_labels_file =
+        fs::read("/home/rondao/train-labels-idx1-ubyte").expect("Could not read images file.");
+
+    assert_eq!(
+        u32::from_be_bytes(training_labels_file[..4].try_into().unwrap()),
+        2049
+    );
+    let _num_images = u32::from_be_bytes(training_labels_file[4..8].try_into().unwrap());
+
+    return training_labels_file[8..].to_vec();
 }
